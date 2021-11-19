45a962ad28b7f21207445488bec2055a4eeb9ef3
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2021-11-20 00:43:45 +0100

    Simplify package.json by specifying a dist method in the docs Makefile

--- docs/api/conf.py
+++ docs/api/conf.py
@@ -57,7 +57,7 @@ templates_path = ['./build/docs-assets/_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [ ]
+exclude_patterns = [ 'index.md' ]
 
 # -- Options for HTML output -------------------------------------------------
 
