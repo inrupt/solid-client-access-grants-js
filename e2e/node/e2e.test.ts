e472dc9215d98cfa78180ae077981c9b21d5aff4
Nicolas Seydoux <nseydoux@inrupt.com>
2022-01-31 11:12:43 +0100

    Add setup and teardown to tests
    The tests now create a shared file and delete it when done.

--- e2e/node/e2e.test.ts
+++ e2e/node/e2e.test.ts
@@ -1,4 +1,20 @@
-import { describe, it, expect, beforeEach, afterEach } from "@jest/globals";
+import {
+  describe,
+  it,
+  expect,
+  beforeEach,
+  afterEach,
+  beforeAll,
+  afterAll,
+} from "@jest/globals";
+import {
+  saveFileInContainer,
+  getPodUrlAll,
+  getSourceUrl,
+  deleteFile,
+} from "@inrupt/solid-client";
+import { Session } from "@inrupt/solid-client-authn-node";
+import { isVerifiableCredential } from "@inrupt/solid-client-vc";
 import {
   approveAccessRequest,
   getAccessGrantAll,
@@ -7,8 +23,6 @@ import {
   revokeAccessGrant,
   getFile,
 } from "../../src/index";
-import { Session } from "@inrupt/solid-client-authn-node";
-import { isVerifiableCredential } from "@inrupt/solid-client-vc";
 import { getTestingEnvironment } from "../e2e-setup";
 
 const {
@@ -19,9 +33,6 @@ const {
   vcProvider,
 } = getTestingEnvironment();
 
-const SHARED_FILE_IRI =
-  "https://storage.dev-next.inrupt.com/eb2f327b-7bb4-4ba2-9b4b-678a4d7e3551/somefile.txt";
-
 // This is the content of the file uploaded manually at SHARED_FILE_IRI.
 const SHARED_FILE_CONTENT = "Some content.\n";
 
@@ -29,7 +40,11 @@ describe(`End-to-end access grant tests for environment [${environment}}]`, () =
   const requestorSession = new Session();
   const resourceOwnerSession = new Session();
 
-  beforeEach(async () => {
+  let sharedFileIri: string;
+
+  // Setup the shared file
+  beforeAll(async () => {
+    // Log both sessions in.
     await requestorSession.login({
       oidcIssuer,
       clientId: requestor.id,
@@ -43,205 +58,235 @@ describe(`End-to-end access grant tests for environment [${environment}}]`, () =
       clientId: resourceOwner.id,
       clientSecret: resourceOwner.secret,
     });
+
+    // Create a file in the resource owner's Pod
+    const resourceOwnerPodAll = await getPodUrlAll(
+      resourceOwnerSession.info.webId as string
+    );
+    if (resourceOwnerPodAll.length === 0) {
+      throw new Error(
+        "The Resource Owner WebID Profile is missing a link to at least one Pod root."
+      );
+    }
+    const savedFile = await saveFileInContainer(
+      resourceOwnerPodAll[0],
+      Buffer.from(SHARED_FILE_CONTENT),
+      {
+        // The session ID is a random string, used here as a unique slug.
+        slug: `${resourceOwnerSession.info.sessionId}.txt`,
+        fetch: resourceOwnerSession.fetch,
+      }
+    );
+    sharedFileIri = getSourceUrl(savedFile);
+    console.log(`Shared file IRI: ${sharedFileIri}`);
   });
 
-  afterEach(async () => {
+  // Cleanup the shared file
+  afterAll(async () => {
+    // Remove the shared file from the resource owner's Pod.
+    deleteFile(sharedFileIri, {
+      fetch: resourceOwnerSession.fetch,
+    });
     // Making sure the session is logged out prevents tests from hanging due
     // to the callback refreshing the access token.
     await requestorSession.logout();
     await resourceOwnerSession.logout();
   });
 
-  it("can issue an access request, grant access to a resource, and revoke the granted access", async () => {
-    const request = await issueAccessRequest(
-      {
-        access: { read: true },
-        requestor: requestorSession.info.webId as string,
-        resourceOwner: resourceOwnerSession.info.webId as string,
-        resources: [SHARED_FILE_IRI],
-        purpose: [
-          "https://some.purpose/not-a-nefarious-one/i-promise",
-          "https://some.other.purpose/",
-        ],
-      },
-      {
-        fetch: requestorSession.fetch,
-        accessEndpoint: vcProvider,
-      }
-    );
-    expect(isVerifiableCredential(request)).toBe(true);
-
-    const grant = await approveAccessRequest(
-      resourceOwnerSession.info.webId as string,
-      request,
-      {},
-      {
-        fetch: resourceOwnerSession.fetch,
-        accessEndpoint: vcProvider,
-      }
-    );
-
-    await expect(
-      isValidAccessGrant(grant, {
-        fetch: resourceOwnerSession.fetch,
-        // FIXME: Currently looking up JSON-LD doesn't work in jest tests.
-        // It is an issue documented in the VC library e2e test, and in a ticket
-        // to be fixed.
-        verificationEndpoint: `${vcProvider}/verify`,
-      })
-    ).resolves.toMatchObject({ errors: [] });
-
-    const grantedAccess = await getAccessGrantAll(SHARED_FILE_IRI, undefined, {
-      fetch: resourceOwnerSession.fetch,
-      accessEndpoint: vcProvider,
-    });
-
-    // Test that looking up the access grants for the given resource returns
-    // the access we just granted.
-    expect(grantedAccess).toContainEqual(grant);
-
-    // For some reason, the Node jest runner throws an undefined error when
-    // calling to btoa. This overrides it, while keeping the actual code
-    // environment-agnostic.
-    global.btoa = (str: string) => Buffer.from(str).toString("base64");
-
-    const sharedFile = await getFile(SHARED_FILE_IRI, grant, {
-      fetch: requestorSession.fetch,
-    });
-    await expect(sharedFile.text()).resolves.toBe(SHARED_FILE_CONTENT);
-
-    await revokeAccessGrant(grant, {
-      fetch: resourceOwnerSession.fetch,
-    });
-    expect(
-      (
-        await isValidAccessGrant(grant, {
-          fetch: resourceOwnerSession.fetch,
-          // FIXME: Ditto verification endpoint discovery.
-          verificationEndpoint: `${vcProvider}/verify`,
-        })
-      ).errors
-    ).toHaveLength(1);
-  });
-
-  it("can filter VCs held by the service based on requestor", async () => {
-    await expect(
-      getAccessGrantAll(
-        SHARED_FILE_IRI,
-        { requestor: requestorSession.info.webId as string },
+  describe("access request, grant and exercise flow", () => {
+    it("can issue an access request, grant access to a resource, and revoke the granted access", async () => {
+      const request = await issueAccessRequest(
         {
-          fetch: resourceOwnerSession.fetch,
-          accessEndpoint: vcProvider,
-        }
-      )
-    ).resolves.not.toHaveLength(0);
-    await expect(
-      getAccessGrantAll(
-        SHARED_FILE_IRI,
-        { requestor: "https://some.unknown.requestor" },
-        {
-          fetch: resourceOwnerSession.fetch,
-          accessEndpoint: vcProvider,
-        }
-      )
-    ).resolves.toHaveLength(0);
-  });
-
-  it("can filter VCs held by the service based on target resource", async () => {
-    await expect(
-      getAccessGrantAll(SHARED_FILE_IRI, undefined, {
-        fetch: resourceOwnerSession.fetch,
-        accessEndpoint: vcProvider,
-      })
-    ).resolves.not.toHaveLength(0);
-    await expect(
-      getAccessGrantAll("https://some.unkown.resource", undefined, {
-        fetch: resourceOwnerSession.fetch,
-        accessEndpoint: vcProvider,
-      })
-    ).resolves.toHaveLength(0);
-  });
-
-  it("can filter VCs held by the service based on purpose", async () => {
-    const [
-      noPurposeFilter,
-      partialPurposeFilter,
-      otherPartialPurposeFilter,
-      bothPurposeFilter,
-      unknownPurposeFilter,
-    ] = await Promise.all([
-      getAccessGrantAll(SHARED_FILE_IRI, undefined, {
-        fetch: resourceOwnerSession.fetch,
-        accessEndpoint: vcProvider,
-      }),
-      getAccessGrantAll(
-        SHARED_FILE_IRI,
-        { purpose: ["https://some.purpose/not-a-nefarious-one/i-promise"] },
-        {
-          fetch: resourceOwnerSession.fetch,
-          accessEndpoint: vcProvider,
-        }
-      ),
-      getAccessGrantAll(
-        SHARED_FILE_IRI,
-        { purpose: ["https://some.other.purpose/"] },
-        {
-          fetch: resourceOwnerSession.fetch,
-          accessEndpoint: vcProvider,
-        }
-      ),
-      getAccessGrantAll(
-        SHARED_FILE_IRI,
-        {
+          access: { read: true },
+          requestor: requestorSession.info.webId as string,
+          resourceOwner: resourceOwnerSession.info.webId as string,
+          resources: [sharedFileIri],
           purpose: [
             "https://some.purpose/not-a-nefarious-one/i-promise",
             "https://some.other.purpose/",
           ],
         },
         {
-          fetch: resourceOwnerSession.fetch,
+          fetch: requestorSession.fetch,
           accessEndpoint: vcProvider,
         }
-      ),
-      getAccessGrantAll(
-        SHARED_FILE_IRI,
-        { purpose: ["https://some.unknown.purpose/"] },
+      );
+      expect(isVerifiableCredential(request)).toBe(true);
+
+      const grant = await approveAccessRequest(
+        resourceOwnerSession.info.webId as string,
+        request,
+        {},
         {
           fetch: resourceOwnerSession.fetch,
           accessEndpoint: vcProvider,
         }
-      ),
-    ]);
+      );
 
-    // All filters should return a result except for the last one.
-    expect(noPurposeFilter.length).toBeGreaterThan(0);
-    expect(partialPurposeFilter.length).toBeGreaterThan(0);
-    expect(otherPartialPurposeFilter.length).toBeGreaterThan(0);
-    expect(bothPurposeFilter.length).toBeGreaterThan(0);
-    expect(unknownPurposeFilter).toHaveLength(0);
-    // The unfiltered results should contain the other ones
-    // Note that we serialize the VCs to avoid comparing by reference
-    expect(
-      bothPurposeFilter.every((vc) =>
-        noPurposeFilter
-          .map((vc) => JSON.stringify(vc))
-          .includes(JSON.stringify(vc))
-      )
-    ).toBe(true);
-    // Filtering on both purposes should include the results filtered on individual purposes
-    expect(
-      partialPurposeFilter.every((vc) =>
-        bothPurposeFilter
-          .map((vc) => JSON.stringify(vc))
-          .includes(JSON.stringify(vc))
-      )
-    ).toBe(true);
-    expect(
-      otherPartialPurposeFilter.every((vc) =>
-        bothPurposeFilter
-          .map((vc) => JSON.stringify(vc))
-          .includes(JSON.stringify(vc))
-      )
-    ).toBe(true);
+      await expect(
+        isValidAccessGrant(grant, {
+          fetch: resourceOwnerSession.fetch,
+          // FIXME: Currently looking up JSON-LD doesn't work in jest tests.
+          // It is an issue documented in the VC library e2e test, and in a ticket
+          // to be fixed.
+          verificationEndpoint: `${vcProvider}/verify`,
+        })
+      ).resolves.toMatchObject({ errors: [] });
+
+      const grantedAccess = await getAccessGrantAll(sharedFileIri, undefined, {
+        fetch: resourceOwnerSession.fetch,
+        accessEndpoint: vcProvider,
+      });
+
+      // Test that looking up the access grants for the given resource returns
+      // the access we just granted.
+      expect(grantedAccess).toContainEqual(grant);
+
+      // For some reason, the Node jest runner throws an undefined error when
+      // calling to btoa. This overrides it, while keeping the actual code
+      // environment-agnostic.
+      global.btoa = (str: string) => Buffer.from(str).toString("base64");
+
+      const sharedFile = await getFile(sharedFileIri, grant, {
+        fetch: requestorSession.fetch,
+      });
+      await expect(sharedFile.text()).resolves.toBe(SHARED_FILE_CONTENT);
+
+      await revokeAccessGrant(grant, {
+        fetch: resourceOwnerSession.fetch,
+      });
+      expect(
+        (
+          await isValidAccessGrant(grant, {
+            fetch: resourceOwnerSession.fetch,
+            // FIXME: Ditto verification endpoint discovery.
+            verificationEndpoint: `${vcProvider}/verify`,
+          })
+        ).errors
+      ).toHaveLength(1);
+    });
+  });
+
+  describe("resource owner interaction with VC provider", () => {
+    it("can filter VCs held by the service based on requestor", async () => {
+      await expect(
+        getAccessGrantAll(
+          sharedFileIri,
+          { requestor: requestorSession.info.webId as string },
+          {
+            fetch: resourceOwnerSession.fetch,
+            accessEndpoint: vcProvider,
+          }
+        )
+      ).resolves.not.toHaveLength(0);
+      await expect(
+        getAccessGrantAll(
+          sharedFileIri,
+          { requestor: "https://some.unknown.requestor" },
+          {
+            fetch: resourceOwnerSession.fetch,
+            accessEndpoint: vcProvider,
+          }
+        )
+      ).resolves.toHaveLength(0);
+    });
+
+    it("can filter VCs held by the service based on target resource", async () => {
+      await expect(
+        getAccessGrantAll(sharedFileIri, undefined, {
+          fetch: resourceOwnerSession.fetch,
+          accessEndpoint: vcProvider,
+        })
+      ).resolves.not.toHaveLength(0);
+      await expect(
+        getAccessGrantAll("https://some.unkown.resource", undefined, {
+          fetch: resourceOwnerSession.fetch,
+          accessEndpoint: vcProvider,
+        })
+      ).resolves.toHaveLength(0);
+    });
+
+    it("can filter VCs held by the service based on purpose", async () => {
+      const [
+        noPurposeFilter,
+        partialPurposeFilter,
+        otherPartialPurposeFilter,
+        bothPurposeFilter,
+        unknownPurposeFilter,
+      ] = await Promise.all([
+        getAccessGrantAll(sharedFileIri, undefined, {
+          fetch: resourceOwnerSession.fetch,
+          accessEndpoint: vcProvider,
+        }),
+        getAccessGrantAll(
+          sharedFileIri,
+          { purpose: ["https://some.purpose/not-a-nefarious-one/i-promise"] },
+          {
+            fetch: resourceOwnerSession.fetch,
+            accessEndpoint: vcProvider,
+          }
+        ),
+        getAccessGrantAll(
+          sharedFileIri,
+          { purpose: ["https://some.other.purpose/"] },
+          {
+            fetch: resourceOwnerSession.fetch,
+            accessEndpoint: vcProvider,
+          }
+        ),
+        getAccessGrantAll(
+          sharedFileIri,
+          {
+            purpose: [
+              "https://some.purpose/not-a-nefarious-one/i-promise",
+              "https://some.other.purpose/",
+            ],
+          },
+          {
+            fetch: resourceOwnerSession.fetch,
+            accessEndpoint: vcProvider,
+          }
+        ),
+        getAccessGrantAll(
+          sharedFileIri,
+          { purpose: ["https://some.unknown.purpose/"] },
+          {
+            fetch: resourceOwnerSession.fetch,
+            accessEndpoint: vcProvider,
+          }
+        ),
+      ]);
+
+      // All filters should return a result except for the last one.
+      expect(noPurposeFilter.length).toBeGreaterThan(0);
+      expect(partialPurposeFilter.length).toBeGreaterThan(0);
+      expect(otherPartialPurposeFilter.length).toBeGreaterThan(0);
+      expect(bothPurposeFilter.length).toBeGreaterThan(0);
+      expect(unknownPurposeFilter).toHaveLength(0);
+      // The unfiltered results should contain the other ones
+      // Note that we serialize the VCs to avoid comparing by reference
+      expect(
+        bothPurposeFilter.every((vc) =>
+          noPurposeFilter
+            .map((vc) => JSON.stringify(vc))
+            .includes(JSON.stringify(vc))
+        )
+      ).toBe(true);
+      // Filtering on both purposes should include the results filtered on individual purposes
+      expect(
+        partialPurposeFilter.every((vc) =>
+          bothPurposeFilter
+            .map((vc) => JSON.stringify(vc))
+            .includes(JSON.stringify(vc))
+        )
+      ).toBe(true);
+      expect(
+        otherPartialPurposeFilter.every((vc) =>
+          bothPurposeFilter
+            .map((vc) => JSON.stringify(vc))
+            .includes(JSON.stringify(vc))
+        )
+      ).toBe(true);
+    });
   });
 });
