46a17523c4d2a42b7221567ee13ae2d7be023604
Nicolas Seydoux <nseydoux@inrupt.com>
2022-02-09 09:42:47 +0100

    Update web footer copyright

--- e2e/browser/src/config.js
+++ e2e/browser/src/config.js
@@ -23,7 +23,7 @@ import packageJson from "./package.json";
 export default () => ({
   libraryRepoUrl: packageJson.repository.url,
   demoRepoUrl: packageJson.repository.url,
-  copyright: "Copyright 2021 Inrupt, Inc.",
+  copyright: "Copyright 2022 Inrupt, Inc.",
   demoTitle: "Web App Template Demo",
   demoDescription: "A template for building in-browser demos",
 });
