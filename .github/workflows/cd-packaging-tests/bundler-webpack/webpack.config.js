2293d1dfccd7898487f1857636f675525c927f91
Zwifi <nseydoux@inrupt.com>
2022-03-01 14:03:57 +0100

    Fix release e2e tests (#115)
    The end-to-end test step of the release workflow was still relying on the legacy environment setup. It is now up-to-date. The configuration of the bundlers used to test the deployments has also been simplified, and a README has been added to explain what is still needed.

--- .github/workflows/cd-packaging-tests/bundler-webpack/webpack.config.js
+++ .github/workflows/cd-packaging-tests/bundler-webpack/webpack.config.js
@@ -1,14 +1,9 @@
-// It should be possible to delete this file once solid-client-authn-browser has switched to jose@3:
+// N3 requires some polyfills
 module.exports = {
   resolve: {
     fallback: {
-      crypto: require.resolve("crypto-browserify"),
       stream: require.resolve("stream-browserify"),
-      util: require.resolve("util/"),
       buffer: require.resolve("buffer/"),
-      os: require.resolve("os-browserify/browser"),
-      assert: require.resolve("assert/"),
-      zlib: require.resolve("browserify-zlib"),
     },
   },
 };
