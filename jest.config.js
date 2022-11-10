543ecff91fd9ae707aaf09f112102de16b85d1d9
Nicolas Seydoux <nseydoux@inrupt.com>
2022-11-10 12:50:19 +0100

    Update jest config for ESM transform

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  preset: "ts-jest",
+  // This combination of preset/transformIgnorePatterns enforces that both TS and
+  // JS files are transformed to CJS, and that the transform also applies to the
+  // dependencies in the node_modules, so that ESM-only dependencies are supported.
+  preset: "ts-jest/presets/js-with-ts",
+  // deliberately set to an empty array to allow including node_modules when transforming code:
+  transformIgnorePatterns: ["/node_modules/(?!jose/dist/browser/)"],
+  modulePathIgnorePatterns: ["dist/", "<rootDir>/examples/"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   clearMocks: true,
   injectGlobals: false,
