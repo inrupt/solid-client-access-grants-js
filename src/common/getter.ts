499a01040903989d7ce836ba37d679196089b795
Nicolas Seydoux <nseydoux@inrupt.com>
2022-11-15 00:18:50 +0100

    Add TSDoc

--- src/common/getter.ts
+++ src/common/getter.ts
@@ -24,6 +24,18 @@ import { ResourceAccessMode } from "../type/ResourceAccessMode";
 import { isAccessGrant as isGConsentAccessGrant } from "../gConsent/guard/isAccessGrant";
 import { AccessModes, resourceAccessToAccessMode } from "../type/AccessModes";
 
+/**
+ * Get the resources to which an Access Grant/Request applies.
+ *
+ * @example
+ *
+ * ```
+ * const resources = getResources(gConsentGrant);
+ * ```
+ *
+ * @param vc The Access Grant/Request
+ * @returns The VC IRI
+ */
 export function getResources(
   vc: AccessGrantGConsent | AccessRequestGConsent
 ): string[] {
@@ -33,10 +45,22 @@ export function getResources(
   return vc.credentialSubject.hasConsent.forPersonalData;
 }
 
+/**
+ * Get the resource owner granting access to their resources from an Access Grant/Request.
+ *
+ * @example
+ *
+ * ```
+ * const ownerWebId = getResourceOwner(gConsentGrant);
+ * ```
+ *
+ * @param vc The Access Grant/Request
+ * @returns The resource owner WebID
+ */
 export function getResourceOwner(vc: AccessGrantGConsent): string;
 export function getResourceOwner(
   vc: AccessGrantGConsent | AccessRequestGConsent
-): undefined;
+): string | undefined;
 export function getResourceOwner(
   vc: AccessGrantGConsent | AccessRequestGConsent
 ): string | undefined {
@@ -46,6 +70,18 @@ export function getResourceOwner(
   return vc.credentialSubject.hasConsent.isConsentForDataSubject;
 }
 
+/**
+ * Get the requestor asking for access to a resources with an Access Grant/Request.
+ *
+ * @example
+ *
+ * ```
+ * const requestorWebId = getRequestor(gConsentGrant);
+ * ```
+ *
+ * @param vc The Access Grant/Request
+ * @returns The requestor WebID
+ */
 export function getRequestor(
   vc: AccessGrantGConsent | AccessRequestGConsent
 ): string {
@@ -55,6 +91,18 @@ export function getRequestor(
   return vc.credentialSubject.id;
 }
 
+/**
+ * Get the access modes granted to a resources via an Access Grant/Request.
+ *
+ * @example
+ *
+ * ```
+ * const modes = getAccessModes(gConsentGrant);
+ * ```
+ *
+ * @param vc The Access Grant/Request
+ * @returns The access modes the grant recipient can exercise.
+ */
 export function getAccessModes(
   vc: AccessGrantGConsent | AccessRequestGConsent
 ): AccessModes {
@@ -66,32 +114,92 @@ export function getAccessModes(
   return resourceAccessToAccessMode(vc.credentialSubject.hasConsent.mode);
 }
 
+/**
+ * Get the ID (URL) of an Access Grant/Request.
+ *
+ * @example
+ *
+ * ```
+ * const id = getId(gConsentGrant);
+ * ```
+ *
+ * @param vc The Access Grant/Request
+ * @returns The VC ID URL
+ */
 export function getId(vc: AccessGrantGConsent | AccessRequestGConsent): string {
   return vc.id;
 }
 
+/**
+ * Get the VC types of an Access Grant/Request.
+ *
+ * @example
+ *
+ * ```
+ * const types = getTypes(gConsentGrant);
+ * ```
+ *
+ * @param vc The Access Grant/Request
+ * @returns The VC types
+ */
 export function getTypes(
   vc: AccessGrantGConsent | AccessRequestGConsent
 ): string[] {
   return vc.type;
 }
 
+/**
+ * Get the issuance date of an Access Grant/Request.
+ *
+ * @example
+ *
+ * ```
+ * const date = getIssuanceDate(gConsentGrant);
+ * ```
+ *
+ * @param vc The Access Grant/Request
+ * @returns The issuance date
+ */
 export function getIssuanceDate(
   vc: AccessGrantGConsent | AccessRequestGConsent
 ): Date | undefined {
   return vc.issuanceDate ? new Date(vc.issuanceDate) : undefined;
 }
 
+/**
+ * Get the expiration date of an Access Grant/Request.
+ *
+ * @example
+ *
+ * ```
+ * const date = getExpirationDate(gConsentGrant);
+ * ```
+ *
+ * @param vc The Access Grant/Request
+ * @returns The issuance date
+ */
 export function getExpirationDate(
   vc: AccessGrantGConsent | AccessRequestGConsent
 ): Date | undefined {
   return vc.expirationDate ? new Date(vc.expirationDate) : undefined;
 }
 
+/**
+ * Get the issuer of an Access Grant/Request.
+ *
+ * @example
+ *
+ * ```
+ * const date = getExpirationDate(gConsentGrant);
+ * ```
+ *
+ * @param vc The Access Grant/Request
+ * @returns The VC issuer
+ */
 export function getIssuer(
   vc: AccessGrantGConsent | AccessRequestGConsent
 ): string {
-  throw new Error("unimplemented");
+  return vc.issuer;
 }
 
 /**
