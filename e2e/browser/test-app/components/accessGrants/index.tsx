356306deb00e48f86840b87f319f53b71d0a9126
Chelsea <4137008+chelseapinka@users.noreply.github.com>
2023-06-08 09:04:41 -0500

    chore(license): Readd license header

--- e2e/browser/test-app/components/accessGrants/index.tsx
+++ e2e/browser/test-app/components/accessGrants/index.tsx
@@ -1,3 +1,24 @@
+//
+// Copyright 2022 Inrupt Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+// Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
 import { getDefaultSession } from "@inrupt/solid-client-authn-browser";
 
 import {
@@ -12,7 +33,7 @@ import {
   getSourceUrl,
   deleteFile,
 } from "@inrupt/solid-client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 
 const session = getDefaultSession();
@@ -50,7 +71,7 @@ export default function AccessGrant({
 
     const savedFile = await saveFileInContainer(
       resourceOwnerPodAll[0],
-      new File([SHARED_FILE_CONTENT], "myFile", { type: "text/plain" }),
+      new File([SHARED_FILE_CONTENT], "fileForGrant", { type: "text/plain" }),
       {
         // The session ID is a random string, used here as a unique slug.
         slug: `${session.info.sessionId}.txt`,
