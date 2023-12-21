850d9c5901300a74a3c282d5c2c47a1d82e6b910
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2023-12-21 11:10:54 +0100

    Allow tsx extension

--- e2e/browser/test-app/.eslintrc.js
+++ e2e/browser/test-app/.eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   // ESLint dependencies are resolved in the root package.
-  extends: ["@inrupt/eslint-config-react", "next/core-web-vitals"]
+  extends: ["@inrupt/eslint-config-react", "next/core-web-vitals"],
+  rules: {
+    "react/jsx-filename-extension": [1, { "extensions": [".tsx"] }]
+  }
 }
