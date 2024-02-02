986b83b52eb41d17b12f577bc0c9749b67ed1b29
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2024-02-02 14:33:15 +0100

    Disable webkit e2e test

--- playwright.config.ts
+++ playwright.config.ts
@@ -58,16 +58,17 @@ const config: PlaywrightTestConfig = {
           process.env.CI === "true" ? "in CI" : "locally"
         }. Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36`,
       },
-    },    
-    {
-      name: "WebKit",
-      use: {
-        browserName: "webkit",
-        userAgent: `Browser-based solid-client-access-grant end-to-end tests running ${
-          process.env.CI === "true" ? "in CI" : "locally"
-        }. Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1`,
-      },
     },
+    // SDK-3286: Re-enable webkit test after figuring out playwright flakyness.   
+    // {
+    //   name: "WebKit",
+    //   use: {
+    //     browserName: "webkit",
+    //     userAgent: `Browser-based solid-client-access-grant end-to-end tests running ${
+    //       process.env.CI === "true" ? "in CI" : "locally"
+    //     }. Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1`,
+    //   },
+    // },
   ],
 };
 
