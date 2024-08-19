a1255ba1f92a5ebcf5041eace10b1fb74c0cc7bc
Pete Edwards <edwardsph@users.noreply.github.com>
2024-08-19 08:28:34 +0100

    SDK-3357: Add node 22 to matrix and set as default (#1078)

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,4 @@
-// The following is only possible from Node 18 onwards
-// import pkg from "./package.json" assert { type: "json" };
-
-// Until we only support Node 18+, this should be used instead
-// (see https://rollupjs.org/guide/en/#importing-packagejson) 
-import { createRequire } from 'node:module';
-const require = createRequire(import.meta.url);
-const pkg = require('./package.json');
+import pkg from "./package.json" with { type: "json" };
 
 import typescript from "rollup-plugin-typescript2";
 
@@ -16,8 +9,6 @@ const external = [
 
 const plugins = [
   typescript({
-    // Use our own version of TypeScript, rather than the one bundled with the plugin:
-    typescript: require("typescript"),
     tsconfigOverride: {
       compilerOptions: {
         module: "esnext",
