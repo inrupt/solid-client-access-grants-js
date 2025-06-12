5475de7536a1387f10c6b8428feecf14fb3a86f7
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2025-06-12 14:37:47 +0200

    Update linting config

--- e2e/browser/test-app/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // ESLint dependencies are resolved in the root package.
-  extends: ["next/core-web-vitals"],
-  rules: {
-    "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
-  },
-};
