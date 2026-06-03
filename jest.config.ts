caa6279484566bec551f6b1c3f080895e071de90
Aaron Coburn <aaronc@inrupt.com>
2026-06-03 14:45:55 -0500

    Adjust jest config

--- jest.config.ts
+++ jest.config.ts
@@ -66,13 +66,8 @@ export default {
       // don't load the polyfills
       setupFilesAfterEnv: [],
       slowTestThreshold: 30,
-      transformIgnorePatterns: [
-        "/node_modules/(?!(.+/)*?(jose|uuid)/)",
-      ],
-      transform: {
-        "^.+\\.tsx?$": "ts-jest",
-        "^.+\\.jsx?$": "ts-jest",
-      },
+      transformIgnorePatterns: ["/node_modules/(?!(.+/)*?(jose|uuid)/)"],
+      transform: { "^.+\\.tsx?$": "ts-jest", "^.+\\.jsx?$": "ts-jest" },
     },
   ],
 } as Config;
